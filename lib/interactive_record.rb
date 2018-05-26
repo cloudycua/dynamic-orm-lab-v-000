99634c4fed75d9bf8e6d00be7962eafab9667f0d
Christine Cua <cloudycua@gmail.com>
2018-05-26 19:54:52 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -72,13 +72,13 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
-  def self.find_by(attribute_hash)
+#  def self.find_by(attribute_hash)
   # executes the SQL to find a row by the attribute passed into the method
   # accounts for when an attribute value is an integer
-    value = attribute_hash.values.first
-    formatted_value = value.class == Fixnum ? value : "'#{value}'"
-    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
-    DB[:conn].execute(sql)
-  end
+#    value = attribute_hash.values.first
+#    formatted_value = value.class == Fixnum ? value : "'#{value}'"
+#    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
+#    DB[:conn].execute(sql)
+#  end
 
 end
